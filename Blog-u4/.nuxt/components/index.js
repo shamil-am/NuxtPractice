5ba64d4d5bff9488e834e0559e07b79940c99716
shamil-am <shamil.mammadov1@gmail.com>
2022-03-31 10:53:41 +0400

    nuxtServerInit added

--- Blog-u4/.nuxt/components/index.js
+++ Blog-u4/.nuxt/components/index.js
@@ -1,10 +1,10 @@
-export { default as AdminAddNewPost } from '../../components/admin/AddNewPost.vue'
 export { default as HomeFooter } from '../../components/Home/HomeFooter.vue'
-export { default as CommonBanner } from '../../components/common/Banner.vue'
-export { default as CommonHeader } from '../../components/common/Header.vue'
+export { default as AdminAddNewPost } from '../../components/admin/AddNewPost.vue'
 export { default as Post } from '../../components/post/Post.vue'
 export { default as PostDetail } from '../../components/post/PostDetail.vue'
 export { default as PostList } from '../../components/post/PostList.vue'
+export { default as CommonBanner } from '../../components/common/Banner.vue'
+export { default as CommonHeader } from '../../components/common/Header.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
